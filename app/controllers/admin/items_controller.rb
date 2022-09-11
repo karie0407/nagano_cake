454d32444f03ef58eed3f8beb918ffc05ae9988a
karie0407 <karie0407@users.noreply.github.com>
2022-09-11 14:38:57 +0000

    admin items

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,19 @@
 class Admin::ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params)
+    @item.admin_id = current_admin.id
+    @item.save
+    redirect_to admin_item_path
+  end
+  
+  
+  
+   private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :introduction, :price, :is_active)
+  end
 end
