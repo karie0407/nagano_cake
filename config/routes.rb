8d07cd3ab6c16268d48b4b0bc0950ccf4af5afb9
karie0407 <karie0407@users.noreply.github.com>
2022-09-27 17:12:36 +0000

    レイアウト調整

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+  get 'home/top'
   namespace :admin do
     root to:'homes#top'
     resources :genres,only: [:new, :index, :edit, :create, :update]
@@ -9,6 +10,7 @@ Rails.application.routes.draw do
     resources :customers,only: [:index, :show, :edit, :update]
     resources :orders,only: [:show]
     resources :order_details,only: [:update]
+    get '/admin/sign_out' => 'admin/sessions#destroy'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
