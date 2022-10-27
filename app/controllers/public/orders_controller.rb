ea6161f71f5e5023f4ecbc7f065af5b22bb35bb5
karie0407 <atseridol0505@gmail.com>
2022-10-27 08:02:50 +0000

    注文機能終了

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,34 +5,35 @@ class Public::OrdersController < ApplicationController
 
   def create
     order = Order.new(order_params)
-    current_order.id = current_customer.id
-    cart_items = current_customer.cart_item
+    order.customer_id=current_customer.id
     if order.save
-      cart_items.each do |cart|
+      current_customer.cart_items.each do |cart|
         order_detail = OrderDetail.new
         order_detail.item_id = cart.item_id
         order_detail.order_id = order.id
-        order_detail.amount = cart.item.amount
-        order_item.price = cart.item.price
+        order_detail.amount = cart.amount
+        order_detail.price  =  cart.item.with_tax_price
         order_detail.save
     end
-    cart_items.destroy_all
-    redirect_to orders_confirm_path
+    current_customer.cart_items.destroy_all
+    redirect_to orders_complete_path
     else
      redirect_to new_order_path
     end
   end
   def confirm
-    @total = 0
-    @cart_items=current_customer.cart_items
+    @order = Order.new(order_params)
+    @cart_items=current_customer.cart_items.all
+    @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
+    @order.shipping_cost = 800
+    @total_payment = @total + @order.shipping_cost
   if params[:order][:select_address] == "0"
-  @order = Order.new(order_params)
   @order.postal_code = current_customer.postal_code
   @order.address = current_customer.address
   @order.name = current_customer.full_name
   elsif params[:order][:select_address] == "1"
   @address = Address.find(params[:order][:address_id])
-  @order.postal_code = @address.postal_code
+  @order.postal_code=@address.postal_code
   @order.address = @address.address
   @order.name = @address.name
   elsif params[:order][:select_address] =="2"
@@ -49,6 +50,6 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :shipping_cost, :total_payment, :status, :customer_id)
   end
 end
