8e439532d0fd001406273f6b0f1077dfff1522b2
karie0407 <atseridol0505@gmail.com>
2022-10-31 17:36:42 +0000

    レイアウト調整

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,23 +1,24 @@
 class Admin::OrderDetailsController < ApplicationController
+  before_action :authenticate_admin!
   def update
-    order_detail = OrderDetail.find(params[:id])
-    order = order_detail.order
-    order_details = order.order_details
-    order_detail.making_status = params[:order_detail][:making_status].to_i
-    order_detail.update(order_detail.params)
-    if order_details.where(making_status: "making").count>=1
-       order.status = "making"
-       order.save
+    @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    @order_details = @order.order_details
+    @order_detail.making_status = params[:order_detail][:making_status].to_i
+    @order_detail.update(order_detail_params)
+    if @order_details.where(making_status: "making").count>=1
+       @order.status = "making"
+       @order.save
     end
 
-    if order.order_details.count == order_details.where(making_status: "completed").count
-      order.status ="shipping_preparation"
-      order.save
+    if @order.order_details.count == @order_details.where(making_status: "completed").count
+      @order.status ="shipping_preparation"
+      @order.save
     end
   end
 
   private
-  def order_detail.params
+  def order_detail_params
     params.require(:order_detail).permit(:making_status)
   end
 end
